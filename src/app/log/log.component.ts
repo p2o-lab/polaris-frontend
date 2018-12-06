4af75f4314be0590100b33549d1ce98676ed17de
Markus Graube <markus.graube@tu-dresden.de>
2018-12-06 17:39:19 +0100

    new log downloads

--- src/app/log/log.component.ts
+++ src/app/log/log.component.ts
@@ -11,10 +11,14 @@ import {SettingsService} from '../_services/settings.service';
 export class LogComponent implements OnInit {
   public logs$: Observable<Object>;
     public readonly logUrl: string;
+    public readonly logVariableUrl: string;
+    public readonly logServiceUrl: string;
 
     constructor(private backend: BackendService,
                 private settings: SettingsService) {
-        this.logUrl = this.settings.apiUrl + '/logs';
+        this.logUrl = this.settings.apiUrl + '/logs.json';
+        this.logServiceUrl = this.settings.apiUrl + '/logs/services.json';
+        this.logVariableUrl = this.settings.apiUrl + '/logs/variables.json';
   }
 
   ngOnInit() {
