b414c7bdb3d0a5afad541c69e0b2712de56b5198
Markus Graube <markus.graube@tu-dresden.de>
2018-12-06 17:41:32 +0100

    update error message

--- src/app/module-view/module-view.component.ts
+++ src/app/module-view/module-view.component.ts
@@ -3,6 +3,7 @@ import {BackendService} from '../_services/backend.service';
 import {ModuleInterface} from 'pfe-ree-interface';
 import {MatDialog} from '@angular/material';
 import {ServiceParameterDialogComponent} from '../service-parameter-dialog/service-parameter-dialog.component';
+import {ServiceInterface} from 'pfe-ree-interface/dist/interfaces';
 
 @Component({
   selector: 'app-module-view',
@@ -23,13 +24,14 @@ export class ModuleViewComponent implements OnInit {
     // update modules without replacing the whole object since this would close the accordion
     this.backend.modules.subscribe((modulesUpdates: ModuleInterface[]) => {
       // this.modules = modules;
-      modulesUpdates.forEach((moduleUpdated) => {
+      modulesUpdates.forEach((moduleUpdated: ModuleInterface) => {
         const module = this.modules.find(moduleFind => moduleFind.id === moduleUpdated.id);
         if (module) {
             if (moduleUpdated.services && module.services) {
-                moduleUpdated.services.forEach(serviceUpdated => {
-                    const service = module.services.find(serviceFind => serviceFind.name === serviceUpdated.name);
+                moduleUpdated.services.forEach((serviceUpdated: ServiceInterface) => {
+                    const service: ServiceInterface = module.services.find(serviceFind => serviceFind.name === serviceUpdated.name);
                     service.status = serviceUpdated.status;
+                    service.error = serviceUpdated.error;
                 });
             }
         } else {
