991e4904025dae3ceb3122a81fa3275b6988c375
Markus Graube <markus.graube@tu-dresden.de>
2019-04-01 11:34:26 +0200

    fix test for timeseries
    Signed-off-by: Markus Graube <markus.graube@tu-dresden.de>

--- src/app/time-series-view/time-series-view.component.spec.ts
+++ src/app/time-series-view/time-series-view.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimeSeriesViewComponent } from './time-series-view.component';
+import {MatCardModule, MatGridListModule, MatSnackBarModule} from '@angular/material';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {SettingsService} from '../_services/settings.service';
+import {settingsServiceStub} from '../_services/settings.service.spec';
+import {WebsocketService, websocketServiceStub} from '../_services/websocket.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('TimeSeriesViewComponent', () => {
   let component: TimeSeriesViewComponent;
@@ -8,7 +15,14 @@ describe('TimeSeriesViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TimeSeriesViewComponent ]
+      declarations: [ TimeSeriesViewComponent ],
+        imports: [
+            MatGridListModule, MatCardModule, MatSnackBarModule,
+            NgxChartsModule, HttpClientTestingModule, BrowserAnimationsModule],
+        providers: [
+            {provide: SettingsService, useValue: settingsServiceStub},
+            {provide: WebsocketService, useValue: websocketServiceStub}
+        ]
     })
     .compileComponents();
   }));
