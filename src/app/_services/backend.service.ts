e0d5fbb8e502deb28a6271e1b6c2bf7f28627897
Markus Graube <markus.graube@tu-dresden.de>
2019-05-13 22:50:00 +0200

    add ping timeout for websocket
    Signed-off-by: Markus Graube <markus.graube@tu-dresden.de>

--- src/app/_services/backend.service.ts
+++ src/app/_services/backend.service.ts
@@ -44,6 +44,7 @@ export class BackendService {
 
     private _updatedVariable: BehaviorSubject<UpdatedVariableInterface> = new BehaviorSubject<UpdatedVariableInterface>(undefined);
     public series: SeriesInterface[] = [];
+    private pingTimeout: any;
 
 
     constructor(private http: HttpClient,
@@ -54,10 +55,19 @@ export class BackendService {
                 public recipeService: RecipeService,
                 public playerService: PlayerService) {
 
+        this.connectToWebsocket();
+        this.refreshModules();
+        this.refreshAutoReset();
+    }
+
+    private connectToWebsocket() {
         this.ws.connect(this.settings.apiUrl.replace('http', 'ws'))
             .subscribe((msg) => {
                 const data: { message: string, data: any } = JSON.parse(msg.data);
                 // console.log('ws received', data.message, data.data);
+                if (data.message === 'ping') {
+                    this.heartbeat();
+                }
                 if (data.message === 'recipes') {
                     this.recipeService.refreshRecipes();
                 }
@@ -76,7 +86,7 @@ export class BackendService {
                     }
                 }
                 if (data.message === 'player') {
-                  this.playerService.refreshPlayer(data.data);
+                    this.playerService.refreshPlayer(data.data);
                 }
                 if (data.message === 'action') {
                     if (data.data === 'recipeCompleted') {
@@ -88,11 +98,13 @@ export class BackendService {
                 if (data.message === 'variable') {
                     const name = <string> data.data.variable;
                     const module = <string> data.data.module;
-                    const value  = <number> data.data.value;
+                    const value = <number> data.data.value;
                     const timestamp = new Date(data.data.timestampPfe);
 
                     let m: ModuleVariableInterface = this._var
-                        .find((m: ModuleVariableInterface) => {return (m.moduleName == module)});
+                        .find((m: ModuleVariableInterface) => {
+                            return (m.moduleName == module)
+                        });
                     if (!m) {
                         m = {moduleName: module, variables: []};
                         this._var.push(m);
@@ -109,21 +121,31 @@ export class BackendService {
                     this._updatedVariable.next({module: module, variable: name, value: value, timestamp: timestamp});
 
                     const seriesName = `${module}.${name}`;
-                    let serie = this.series.find(s=> s.name === seriesName);
+                    let serie = this.series.find(s => s.name === seriesName);
 
                     if (serie) {
                         serie.data.push([timestamp.getTime(), value]);
                         const firstTimestamp = serie.data[0][0];
-                        if (new Date().getTime() - firstTimestamp > 1000*60*5) {
+                        if (new Date().getTime() - firstTimestamp > 1000 * 60 * 5) {
                             serie.data.shift();
                         }
                     } else {
                         this.series.push({name: seriesName, data: [[timestamp.getTime(), value]]});
                     }
-             }
+                }
             });
-        this.refreshModules();
-        this.refreshAutoReset();
+    }
+
+    heartbeat() {
+        clearTimeout(this.pingTimeout);
+
+        // Use `WebSocket#terminate()` and not `WebSocket#close()`. Delay should be
+        // equal to the interval at which your server sends out pings plus a
+        // conservative assumption of the latency.
+        this.pingTimeout = setTimeout(() => {
+            console.log("Connection to backend lost");
+            this.snackBar.open("Connection to backend lost");
+        }, 3000 + 1000);
     }
 
     get updatedVariable(): Observable<UpdatedVariableInterface> {
@@ -183,7 +205,7 @@ export class BackendService {
     }
 
     addModule(moduleOptions) {
-        return this.http.put(`${this.settings.apiUrl}/module`, {modules: moduleOptions});
+        return this.http.put(`${this.settings.apiUrl}/module`, {module: moduleOptions});
     }
 
     connect(module: string) {
@@ -216,5 +238,9 @@ export class BackendService {
         const formData: FormData = new FormData();
         formData.append('upload', file);
         return this.http.post(`${this.settings.mtpConverterUrl}/json`, formData);
+    }
+
+    public shutdown() {
+        return this.http.post(`${this.settings.apiUrl}/shutdown`, null);
     }
 }
