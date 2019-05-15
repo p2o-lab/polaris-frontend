0feb2c30c19b152db03957c8d3b3600fa84ee492
Markus Graube <markus.graube@tu-dresden.de>
2019-05-15 11:26:52 +0200

    add default port to new module if missing
    Signed-off-by: Markus Graube <markus.graube@tu-dresden.de>

--- src/app/new-module/new-module.component.ts
+++ src/app/new-module/new-module.component.ts
@@ -25,16 +25,36 @@ export class NewModuleComponent {
       if (file.name.endsWith('.mtp') || file.name.endsWith('.zip')){
         this.backend.convertMtp(file).subscribe((data) => {
             this.modules = <ModuleOptions[]> data['modules'];
+            let module = this.modules[0];
+
+            const re = new RegExp("(opc\.tcp):\/\/([^:\/\s]+)(:[0-9]+)?(\S*)?");
+            module.opcua_server_url = module.opcua_server_url.replace(re, (match, scheme, host, port, path) => {
+                return `${scheme}://${host}${port||':4840'}${path||''}`
+            });
+
             this.myStepper.next();
         })
       } else {
           const reader: FileReader = new FileReader();
           reader.onload = (e: Event) => {
               this.modules = <ModuleOptions[]> JSON.parse(reader.result.toString()).modules;
+              let module = this.modules[0];
+              module.opcua_server_url = this.addDefaultPort(module.opcua_server_url);
               this.myStepper.next();
           };
           reader.readAsText(file);
       }
+  }
+
+    /**
+     * Add default port (4840) to server url if not present
+     * @param opcua_server_url
+     */
+  public addDefaultPort(opcua_server_url: string): string {
+      const re = new RegExp("(opc\.tcp):\/\/([^:\/]+)(:[0-9]+)?(\S*)?");
+      return opcua_server_url.replace(re, (match, scheme, host, port, path) => {
+          return `${scheme}://${host}${port||':4840'}${path||''}`
+      });
   }
 
   public addModule() {
