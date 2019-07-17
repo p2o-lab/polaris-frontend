c52f356bdc73126471e1b801b7708efda2bdd3d1
Sebastian Heinze <sebastian.heinze1@tu-dresden.de>
2019-07-17 16:03:07 +0200

    linter fixes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -12,15 +12,17 @@ import {
         MatAutocompleteModule, MatBadgeModule, MatButtonModule,
         MatCardModule, MatCheckboxModule, MatChipsModule, MatDialogModule, MatDividerModule, MatExpansionModule,
         MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
-        MatPaginatorModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSnackBarModule,
-        MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule
+        MatPaginatorModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSliderModule,
+        MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule,
+        MatTooltipModule
     ],
     exports: [
         MatAutocompleteModule, MatBadgeModule, MatButtonModule,
         MatCardModule, MatCheckboxModule, MatChipsModule, MatDialogModule, MatDividerModule, MatExpansionModule,
         MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
-        MatPaginatorModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSnackBarModule,
-        MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule
+        MatPaginatorModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSliderModule,
+        MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule,
+        MatTooltipModule
     ],
 })
 export class MaterialModule {
