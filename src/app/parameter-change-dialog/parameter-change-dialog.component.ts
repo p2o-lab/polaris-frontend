6a4928c5f9df012be8a0deae95095c546016be32
Markus Graube <markus.graube@tu-dresden.de>
2019-10-15 13:09:54 +0200

    ignore .vscode

--- src/app/parameter-change-dialog/parameter-change-dialog.component.ts
+++ src/app/parameter-change-dialog/parameter-change-dialog.component.ts
@@ -16,7 +16,7 @@ export class ParameterChangeDialogComponent {
       @Inject(MAT_DIALOG_DATA) public param: ParameterInterface) {
     this.newParamOptions = {
       name: param.name,
-      value: param.value
+      value: param.requestedValue
     };
   }
 
