d99c26bfc31141d065c8afb2564581f1e6d0e3c9
Sebastian Heinze <sebastian.heinze1@tu-dresden.de>
2019-11-27 17:26:00 +0100

    added mising imports for module service unit test

--- src/app/_services/module.service.spec.ts
+++ src/app/_services/module.service.spec.ts
@@ -1,13 +1,17 @@
+import {Overlay} from '@angular/cdk/overlay';
 import {HttpClient} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {inject, TestBed} from '@angular/core/testing';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {LoggerConfig, NGXLogger, NGXLoggerHttpService, NGXMapperService} from 'ngx-logger';
 import {ModuleService} from './module.service';
 import {SettingsService} from './settings.service';
 
 describe('ModuleService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ModuleService, HttpClient, SettingsService],
+      providers: [ModuleService, HttpClient, SettingsService, MatSnackBar, Overlay, NGXLogger, NGXMapperService,
+        LoggerConfig, NGXLoggerHttpService],
       imports: [HttpClientTestingModule]
     });
   });
